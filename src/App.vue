c83fc4dea8a0c5f719832148fe5a4d966bb29afd
LLXXzz <1071327578@qq.com>
2021-10-24 22:21:55 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Lxz></Lxz>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Lxz from "./components/Lxz.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Lxz,
+  },
+};
+</script>
