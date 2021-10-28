697b3411831f76ca9079d6a309f411466733f2ff
LLXXzz <1071327578@qq.com>
2021-10-28 19:02:54 +0800

    topnav组件封装

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,7 @@
 </template>
 
 <script>
-import Lxz from "./components/Lxz.vue";
-import Lxz2 from "./components/Lxz2.vue";
-
 export default {
   name: "App",
-  components: {
-    Lxz,
-    Lxz2,
-  },
 };
 </script>
