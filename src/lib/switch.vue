bf46478e2e9031adb858cbd9b1e5a7d1f9f09891
LLXXzz <1071327578@qq.com>
2021-10-29 19:14:41 +0800

    添加了过渡动画

--- src/lib/switch.vue
+++ src/lib/switch.vue
@@ -35,6 +35,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 // 当button拥有checked属性时
 button.checked {
@@ -42,5 +43,8 @@ button.checked {
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+button:focus {
+  outline: none;
 }
 </style>
