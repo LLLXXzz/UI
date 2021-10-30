67cedd2879b4008ba3c44e3ef7127dd743c5d994
LLXXzz <1071327578@qq.com>
2021-10-30 19:35:32 +0800

    创建dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
     <p>dialogDemo</p>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
   </div>
 </template>
+<script>
+import Dialog from "../lib/dialog.vue";
+
+export default {
+  components: { Dialog },
+  setup() {},
+};
+</script>
---
+++ src/lib/dialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>1</p>
+      <p>2</p>
+    </main>
+    <footer>
+      <Button>取消</Button>
+      <Button>确认</Button>
+    </footer>
+  </div>
+</template>
+<script>
+import Button from "../lib/button.vue";
+export default {
+  components: { Button },
+  setup() {},
+};
+</script>
