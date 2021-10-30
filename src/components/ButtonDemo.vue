bd8259102cbb82249467937f4a2dcf9e9be26265
LLXXzz <1071327578@qq.com>
2021-10-30 17:21:39 +0800

    支持theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,11 @@
 <template>
+  <div>Button 示例</div>
+  <h1>示例一</h1>
   <div>
-    <Button>我是按钮</Button>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="text">你好</Button>
+    <Button theme="link">你好</Button>
   </div>
 </template>
 <script>
