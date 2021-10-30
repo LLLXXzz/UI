07d37b3fafe57f328dd04d1944cb542cdfc151a7
LLXXzz <1071327578@qq.com>
2021-10-30 17:49:36 +0800

    支持size属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -7,6 +7,12 @@
     <Button theme="text">你好</Button>
     <Button theme="link">你好</Button>
   </div>
+  <h1>示例二</h1>
+  <div>
+    <Button size="small">小小小</Button>
+    <Button>普通</Button>
+    <Button size="big">大大大</Button>
+  </div>
 </template>
 <script>
 import Button from "../lib/button.vue";
