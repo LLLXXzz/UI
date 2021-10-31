e9c17946a4de4522170eb5f6b0c6c74dce73c038
LLXXzz <1071327578@qq.com>
2021-10-31 23:24:37 +0800

    indicator

--- src/components/TabDemo.vue
+++ src/components/TabDemo.vue
@@ -2,17 +2,21 @@
   <div>
     <div>Tabs组件</div>
     <h1>示例1</h1>
-    <Tabs>
+    <Tabs v-model:selected="x">
       <Tab title="导航1">内容1</Tab>
       <Tab title="导航2">内容2</Tab>
     </Tabs>
   </div>
 </template>
 <script lang='ts'>
+import { ref } from "vue";
 import Tab from "../lib/tab.vue";
 import Tabs from "../lib/tabs.vue";
 export default {
   components: { Tab, Tabs },
-  setup() {},
+  setup() {
+    const x = ref("导航1");
+    return { x };
+  },
 };
 </script>
--- src/lib/tabs.vue
+++ src/lib/tabs.vue
@@ -1,14 +1,63 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key='index'> 
-        {{t}}
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div
+        @click="select(t)"
+        class="gulu-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+        :ref="
+          (el) => {
+            if (el) navItems[index] = el;
+          }
+        "
+        :class="{ selected: t === selected }"
+      >
+        {{ t }}
+      </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+    <div class="gulu-tabs-content">
+      <component
+        class="gulu-tabs-content-item"
+        :class="{ selected: c.props.title === selected }"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
+  </div>
 </template>
 <script lang='ts'>
 import Tab from "./tab.vue";
+import { onMounted, ref } from "vue";
+
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
+    //<HTMLDivElement[]> 这里的([])是HTML div元素数组
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    //挂载之后
+    onMounted(() => {
+      //打印navItems.value的值 值就是两个div
+      //   console.log(...navItems.value);
+      //获取两个div
+      const divs = navItems.value;
+      //找到class为seleced的div filter过滤
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0];
+      //获取该元素宽度
+      //   const width = result.getBoundingClientRect().width ;
+      const { width } = result.getBoundingClientRect();
+      //让蓝线跟元素一样宽
+      indicator.value.style.width = width + "px";
+    });
     //确定子组件的类型
     const defaults = context.slots.default();
     // console.log(defaults[0].type === Tab);
@@ -17,10 +66,55 @@ export default {
         throw new Error("Tabs 子标签必须是Tab");
       }
     });
+    //获取组件中的标题  t代表每个标题
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    return { defaults, titles };
+    const select = (title) => {
+      context.emit("update:selected", title);
+    };
+    return { defaults, titles, select, navItems, indicator };
   },
 };
 </script>
+<style lang='scss'>
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
+</style>
