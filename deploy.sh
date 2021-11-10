1736ed6acb0cae0decfea39b0cb5f295ca5fdd90
LLXXzz <1071327578@qq.com>
2021-11-10 19:02:36 +0800

    修改deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ rm -rf dist &&
 yarn build  &&
 cd dist     &&
 git init    &&
-git add     &&
+git add .   &&
 git commit -m "update"  &&
 git branch -M master    &&
 git remote add origin git@github.com:LLLXXzz/   guluUI-website.git  &&
