ab5ddeca99d54553d24b3736902ab8d4adf34fef
LLXXzz <1071327578@qq.com>
2021-11-10 22:47:31 +0800

    创建lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,13 @@
+import { openDialog } from './openDialog';
+// import Switch from './switch.vue'
+// export {Switch}
+//简写 
+//把switch默认的东西当成switch导出
+export { default as Switch } from './switch.vue'
+export { default as Button } from './button.vue'
+export { default as Tabs } from './tabs.vue'
+export { default as Tab } from './tab.vue'
+export { default as dialog } from './dialog.vue'
+export { openDialog as openDialog } from './openDialog'
+
+
